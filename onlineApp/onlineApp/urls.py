00518272f64e94c8b37509a83f023dfe27448c03
Khine <khinezar70122107@gmail.com>
2022-11-21 06:56:21 -1200

    first web app

--- onlineApp/onlineApp/urls.py
+++ onlineApp/onlineApp/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('',include('onlineSecondApp.urls')),
     path('admin/', admin.site.urls),
 ]
+#django at first this line 20 url
+#so link
+#path('',include('onlineSecondApp.urls'))
---
+++ onlineApp/onlineSecondApp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+]
+
+#file >> function express
+#path(takes 3 parameters) 
+#first parameter ---> route name
+#second parameter ----> ဘယ် file ထဲက ဘယ်functionဆိုတာ ---> views.py --> home function
+#third parameter ----> name
